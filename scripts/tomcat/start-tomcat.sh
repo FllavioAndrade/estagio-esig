06a32bd5beb248dc1524518c517f02f212992146
fllavioandrade <flavio.a.s@live.com>
2025-02-13 01:40:57 -0300

    Adicionado timestamp no monitoramento

--- scripts/tomcat/start-tomcat.sh
+++ scripts/tomcat/start-tomcat.sh
@@ -5,16 +5,21 @@ NOME_DO_CONTEINER="tomcat-server"
 
 # Reinicia o Container se estiver parado por mais de 1 minuto
 
+timestamp() {
+    date +"%Y-%m-%d %H:%M:%S" 
+}
+
+
 HORA_PARADA=$(docker inspect -f '{{.State.FinishedAt}}' $NOME_DO_CONTEINER)
 TEMPO_PARADO=$(( $(date +%s) - $(date -d "$HORA_PARADA" +%s) ))
 HORA=$((TEMPO_PARADO / 3600))
 MIN=$(((TEMPO_PARADO % 3600)/60))
 SEG=$((TEMPO_PARADO % 60))
 
-echo "A instância está INATIVA"
-echo "Tempo de atividade: $((HORA)) h   $((MIN)) min $((SEG)) seg."
+echo "$(timestamp) | A instância está INATIVA"
+echo "$(timestamp) | Tempo de atividade: $((HORA)) h   $((MIN)) min $((SEG)) seg."
 if [ $TEMPO_PARADO -gt 60 ]; then
   docker start $NOME_DO_CONTEINER
   sleep 5
-  echo "Instância reiniciada com sucesso."
+  echo "$(timestamp) | instancia iniciada com sucesso."
 fi
--- scripts/tomcat/status-tomcat.sh
+++ scripts/tomcat/status-tomcat.sh
@@ -6,6 +6,10 @@ NOME_DO_CONTEINER="tomcat-server"
 # Verificar o status do contêiner e reiniciar se estiver parado por mais de 1 minuto
 STATUS=$(docker inspect -f '{{.State.Status}}' $NOME_DO_CONTEINER)
 
+timestamp() {
+    date +"%Y-%m-%d %H:%M:%S" 
+}
+
 #
 if [ "$STATUS" == "running" ]; then
   TEMPO_ATIVO=$(docker inspect -f '{{.State.StartedAt}}' $NOME_DO_CONTEINER)
@@ -13,8 +17,9 @@ if [ "$STATUS" == "running" ]; then
   HORA=$((SEGUNDOS_ATIVO / 3600))
   MIN=$(((SEGUNDOS_ATIVO % 3600)/60)) 
   SEG=$((SEGUNDOS_ATIVO % 60))
-  echo "A instância do Tomcat está em execução. $SEG" 
-  echo "Tempo de atividade: $((HORA)) h   $((MIN)) min $((SEG)) seg."
+  echo "$(timestamp) | A instância do Tomcat está em execução." 
+  echo "$(timestamp) | Tempo de atividade: $((HORA)) h   $((MIN)) min $((SEG)) seg."
 else
-    ./start-tomcat.sh
+    echo "$(timestamp) | INSTANCIA INATIVA!!"
+    ./start-tomcat.sh >> /home/vagrant/tomcat/log/inativo/log.txt
 fi
